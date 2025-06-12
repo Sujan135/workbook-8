d377e50c8d71ba6fddf42d8cca475263fb8b8ada
Sujan Banjara <banjara.suzan12@gmail.com>
2025-06-12 10:46:31 -0500

    Implement method to call CustOrderHist stored procedure

--- NorthwindProcedures/src/main/java/DataManager.java
+++ NorthwindProcedures/src/main/java/DataManager.java
@@ -1,5 +1,9 @@
 import org.apache.commons.dbcp2.BasicDataSource;
 import javax.sql.DataSource;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DataManager {
     private DataSource dataSource;
@@ -14,5 +18,24 @@ public class DataManager {
 
     public DataSource getDataSource() {
         return dataSource;
+    }
+
+    public void getCustomerOrderHistory(String customerId) {
+        String query = "{CALL CustOrderHist(?)}";
+        try (Connection conn = dataSource.getConnection();
+             CallableStatement stmt = conn.prepareCall(query)) {
+
+            stmt.setString(1, customerId);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                System.out.printf("%s - %d\n",
+                        rs.getString("ProductName"),
+                        rs.getInt("TOTAL"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
